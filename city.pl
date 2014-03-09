e0d76be9ebbc538549229c65fd87cae267fb8009
sue7ga <sue77ga@gmail.com>
2014-03-09 04:23:38 -0700

    city

---
+++ city.pl
@@ -0,0 +1,44 @@
+use strict;
+use warnings;
+
+my $datafile = "city.txt";
+
+open(FH,$datafile);
+
+my @city_list = <FH>;
+my $records = [];
+
+my $pattern_re = qr/
+  ^
+   (\d{6})
+  ：
+   (\w+)
+  $
+/x;
+
+my $record = {};
+
+foreach my $city (@city_list){
+ 
+  chomp($city);
+
+  my $area = $1  if $city =~ /(\w+)(県|地方|府|都)/;
+
+  my $id;
+  my $city;
+
+  if($city =~ /$pattern_re/){
+      $id = $1;
+      $city = $2;
+  }
+
+  $record->{$area}->{$id} = $id;
+  $record->{$area}->{$city} = $city;
+
+  push @$records,$record;
+}
+
+use Data::Dumper;
+
+print Dumper @$records;
+
