b0c3389d2eec770fd5df5c4e49939a517ebcbab1
sue7ga <sue77ga@gmail.com>
2014-03-14 11:16:03 -0700

    all

---
+++ liveweather.pl
@@ -0,0 +1,23 @@
+use strict;
+use warnings;
+
+use JSON;
+use WebService::Simple;
+use utf8;
+
+use Encode qw/encode_utf8 decode_utf8/;
+
+use constant BASEURL => 'http://weather.livedoor.com/forecast/webservice/json/v1';
+
+my $agent = WebService::Simple->new(
+  base_url => 'http://weather.livedoor.com/forecast/webservice/json/v1',
+  response_parser => 'JSON',
+);
+
+
+my $res = $agent->get({city => '400040'});
+my $ref = $res->is_success ? decode_json($res->{_content}) : "";
+
+use Data::Dumper;
+
+print  $ref->{forecasts}->[0]->{date};
